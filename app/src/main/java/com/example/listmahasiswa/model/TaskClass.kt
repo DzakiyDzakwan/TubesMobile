e097d156cc0b0c7eb94d7afe203fcabbdc1c4d1f
Dzakiy Dzakwan <dzakcart@gmail.com>
2023-12-20 01:29:25 +0700

    Main Activity Todo List

--- app/src/main/java/com/example/listmahasiswa/model/TaskClass.kt
+++ app/src/main/java/com/example/listmahasiswa/model/TaskClass.kt
@@ -4,34 +4,36 @@ import android.os.Parcel
 import android.os.Parcelable
 import java.util.Date
 
-data class TaskClass(val id: Int,
+data class TaskClass(val id: Int?,
                      val name: String,
-                     val mark_as_finished: UByte,
-                     val started_at: Date,
-                     val deadline_at: Date,
-                     val created_at: Date,
-                     val updated_at: Date) : Parcelable
+                     val markAsFinished: Boolean,
+                     val startedAt: Date?,
+                     val deadlineAt: Date?,
+                     val createdAt: Date?,
+                     val updatedAt: Date?) : Parcelable
 {
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
         parcel.readString()!!,
-        parcel.readByte().toUByte(),
+        parcel.readByte() != 0.toByte(),
         parcel.readValue(Date::class.java.classLoader) as? Date
             ?: Date(), // Use Date() as a default value if null
         parcel.readValue(Date::class.java.classLoader) as? Date ?: Date(),
         parcel.readValue(Date::class.java.classLoader) as? Date ?: Date(),
         parcel.readValue(Date::class.java.classLoader) as? Date ?: Date()
+
     )
 
-    override fun writeToParcel(parcel: Parcel, flags: Int)
-    {
-        parcel.writeInt(id)
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id ?: 0) // Use 0 as a default value if id is null
         parcel.writeString(name)
-        parcel.writeByte(mark_as_finished.toByte())
-        parcel.writeValue(started_at)
-        parcel.writeValue(deadline_at)
-        parcel.writeValue(created_at)
-        parcel.writeValue(updated_at)
+//        parcel.writeString(description)
+        parcel.writeByte(if (markAsFinished) 1 else 0)
+        // Write nullable dates with null checks
+        parcel.writeLong(startedAt?.time ?: 0L)
+        parcel.writeLong(deadlineAt?.time ?: 0L)
+        parcel.writeLong(createdAt?.time ?: 0L)
+        parcel.writeLong(updatedAt?.time ?: 0L)
     }
 
     override fun describeContents(): Int
