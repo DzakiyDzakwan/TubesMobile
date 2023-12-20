2734ecf63f229107b1ba15fd044de3c7b1fc41fa
HelloitsMutiaa <rahmahm868@gmail.com>
2023-12-20 11:05:31 +0700

    Fix search function

--- app/src/main/java/com/example/listmahasiswa/MainActivity.kt
+++ app/src/main/java/com/example/listmahasiswa/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.SearchView
-//import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.listmahasiswa.api.RetrofitClient
@@ -21,37 +20,24 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
-    //private  lateinit var mahasiswaList: ArrayList<MahasiswaClass>
-
-    private lateinit var mahasiswa: ArrayList<MahasiswaClass>
-    private lateinit var tasks: ArrayList<TaskClass>
+    private lateinit var task: ArrayList<TaskClass>
     private var taskList: ArrayList<TaskClass> = arrayListOf()
-
-    //private lateinit var  myAdapter: AdapterClass
-    private  lateinit var taskAdapter: TaskAdapterClass
+    private lateinit var taskAdapter: TaskAdapterClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val buttonToDoList: android.widget.Button = findViewById(R.id.buttonToDoList)
-//        buttonToDoList.setOnClickListener {
-//            val intent = Intent(this, ToDoListActivity::class.java)
-//            startActivity(intent)
-//        }
-
-        tasks = arrayListOf(
+        task = arrayListOf(
             TaskClass(null, "Tugas 1", true, null, getRandomDate(), Date(), Date()),
             TaskClass(null, "Tugas 2", false, null, getRandomDate(), Date(), Date()),
             TaskClass(null, "Tugas 3", false, null, getRandomDate(), Date(), Date()),
             TaskClass(null, "Tugas 4", false, null, getRandomDate(), Date(), Date()),
             TaskClass(null, "Tugas 5", false, null, getRandomDate(), Date(), Date()),
             TaskClass(null, "Tugas 6", false, null, getRandomDate(), Date(), Date()),
-            // Add more tasks as needed
         )
 
         val apiService = RetrofitClient.apiService
-
         val call = apiService.getData()
 
         call.enqueue(object : Callback<ResponseModel> {
@@ -62,20 +48,14 @@ class MainActivity : AppCompatActivity() {
 
                     tasks?.let {
                         taskList.addAll(it)
-//                        tasks.addAll(it)
-
-                        if(taskList.isEmpty()) {
-                            taskList.addAll(tasks)
-                        }
-
+                        task.addAll(it)
+                        taskAdapter.notifyDataSetChanged()
                         taskList.forEach { task ->
                             Log.d("Task", "ID: ${task.id}, Name: ${task.name}")
                         }
-                        taskAdapter.notifyDataSetChanged()
                     }
                 } else {
                     Log.e("API Response", "Error: ${response.code()}")
-                    taskList.addAll(tasks)
                 }
             }
 
@@ -100,34 +80,22 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
-        // List Mahasiswa
-//        myAdapter = AdapterClass(mahasiswa)
-//        recyclerView.adapter = myAdapter
-//        myAdapter.onItemClick = {
-//            val intent = Intent(this, DetailActivity::class.java)
-//            intent.putExtra("android", it)
-//            startActivity(intent)
-//        }
-
         taskAdapter = TaskAdapterClass(taskList)
-
         recyclerView.adapter = taskAdapter
         taskAdapter.onItemClick = {
             val intent = Intent(this, TaskDetail::class.java)
             intent.putExtra("taskId", it)
             startActivity(intent)
-
         }
 
         val buttonAdd = findViewById<FloatingActionButton>(R.id.buttonAddTodo)
-
         buttonAdd.setOnClickListener {
             val intent = Intent(this, CreateTaskActivity::class.java)
             startActivity(intent)
         }
     }
 
-   private fun getRandomDate(): Date {
+    private fun getRandomDate(): Date {
         val calendar = Calendar.getInstance()
         calendar.add(Calendar.DAY_OF_YEAR, Random.nextInt(1, 30)) // Random day between 1 and 30
         return calendar.time
@@ -136,9 +104,9 @@ class MainActivity : AppCompatActivity() {
     private fun filterTaskList(query: String?) {
         taskList.clear()
         if (query.isNullOrBlank()) {
-            taskList.addAll(tasks)
+            taskList.addAll(task)
         } else {
-            val filteredList = tasks.filter { it.name.contains(query, ignoreCase = true) }
+            val filteredList = task.filter { it.name.contains(query, ignoreCase = true) }
             taskList.addAll(filteredList)
         }
         taskAdapter.notifyDataSetChanged()
